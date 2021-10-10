6ec872bfe01edc9cf69b638b35804331debac04e
Santosh Pattnaik <santoshpattnaik91@gmail.com>
2021-10-10 21:20:30 +0530

    Deleted some unused comments

--- data_fetch.php
+++ data_fetch.php
@@ -2,18 +2,43 @@
 
 require_once DIR . '/db_fetch.php';
 
-/**
- * Variable to fetch date
- * 
- * @var string $date
- */
 
 while ($row = $result->fetch_assoc()) {
+    /**
+     * Variable to fetch date
+     * 
+     * @var string $date
+     */
     $date = $row['Date'];
+
+    /**
+     * Variable to fetch account holder name
+     * 
+     * @var string $acname
+     */
     $acname = $row['Acname'];
+
+    /**
+     * Variable to fetch transaction ID
+     * 
+     * @var string $trans_id
+     */
     $trans_id = $row['Trans_ID'];
+
+    /**
+     * Variable to fetch amount of transaction
+     * 
+     * @var string $amount
+     */
     $amount = $row['Amount'];
+
+    /**
+     * Variable to fetch the balance amount in the account
+     * 
+     * @var string $balance
+     */
     $balance = $row['Balance'];
+
     echo
     "<tr>
                         <td>
@@ -33,31 +58,3 @@ while ($row = $result->fetch_assoc()) {
                         </td>   
                     </tr>";
 }
-
-/**
- * Variable to fetch account holder name
- * 
- * @var string $acname
- */
-// $acname = 'Santosh Pattnaik';
-
-/**
- * Variable to fetch transaction ID
- * 
- * @var string $trans_id
- */
-// $trans_id = 'abcd';
-
-/**
- * Variable to fetch amount of transaction
- * 
- * @var string $amount
- */
-// $amount = '10,000';
-
-/**
- * Variable to fetch the balance amount in the account
- * 
- * @var string $balance
- */
-// $balance = '1,00,000';
