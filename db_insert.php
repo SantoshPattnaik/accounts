1b6b4d0893f520b0ff729b06d66a71d0dc31f509
Santosh Pattnaik <santoshpattnaik91@gmail.com>
2021-10-29 15:39:04 +0530

    added some input string filtering

--- db_insert.php
+++ db_insert.php
@@ -6,13 +6,13 @@ $db_sl_no = $result->num_rows;
 
 $sl_no = ++$db_sl_no;
 
-$date_input = remove_special_chars($_POST['date']);
-$acname_input = remove_special_chars($_POST['acname']);
-$bank = remove_special_chars($_POST['bank']);
-$payment_method = remove_special_chars($_POST['pay_method']);
-$trans_id_input = remove_special_chars($_POST['trans_id']);
-$amount_input = remove_special_chars($_POST['amount']);
-$balance_input = remove_special_chars($_POST['balance']);
+$date_input = string_filter($_POST['date']);
+$acname_input = string_filter($_POST['acname']);
+$bank = string_filter($_POST['bank']);
+$payment_method = string_filter($_POST['pay_method']);
+$trans_id_input = string_filter($_POST['trans_id']);
+$amount_input = string_filter($_POST['amount']);
+$balance_input = string_filter($_POST['balance']);
 
 if (!empty($date_input)) {
     $insertion_query = "INSERT INTO santosh_pattnaik VALUES ('$sl_no','$date_input','$acname_input','$bank','$payment_method','$trans_id_input','$amount_input','$balance_input')";
--- filters.php
+++ filters.php
@@ -1,8 +1,21 @@
 <?php
 
+function string_filter($str)
+{
+    $htmlremoved = remove_htmlTags($str);
+
+    return remove_special_chars($htmlremoved);
+}
+
 function remove_special_chars($str)
 {
-    $specialchars = array('/', '\'', ',', '.', '\"', ';', ':', '|', '\\', '<', '>', '?');
+    $regex = '/[^a-zA-Z0-9 ]/';
+    return preg_replace($regex, '', $str);
+}
 
-    return str_replace($specialchars, '', $str);
+function remove_htmlTags($str)
+{
+    /*$regex = '@<(script|style|h1)[^>]*?>.*?</\\1>@si';*/
+// return preg_replace($regex,'',$str);
+return strip_tags($str);
 }
