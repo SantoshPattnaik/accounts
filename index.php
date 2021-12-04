99b7f5755f7366bde600a7470498efef88065556
Santosh Pattnaik <santoshpattnaik91@gmail.com>
2021-12-04 20:35:20 +0530

    Totally changed the index page and added another management page for accounts listing

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php require_once './constants.php';
-require_once './functions.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,81 +11,74 @@ require_once './functions.php' ?>
 </head>
 
 <body>
-    <h1 id="site-title" class="display-3">ACCOUNTS MANAGEMENT</h1>
-    <div class="table-responsive">
-        <form class="form-group" name="mainForm" method="POST" onsubmit="return validate()">
-            <table aria-label="table" id="table" class="table table-striped table-bordered">
-                <thead class="table-dark text-center">
-                    <tr>
-                        <th id="date" scope="col">Date of Transaction</th>
-                        <th id="account_name" scope="col">Account Holder Name</th>
-                        <th id="bank_name" scope="col">Bank Name</th>
-                        <th id="payment_method" scope="col">Payment Method</th>
-                        <th id="trans_id" scope="col">Transaction ID</th>
-                        <th id="amount" scope="col">Amount</th>
-                        <th id="balance" scope="col">Balance</th>
-                    </tr>
-                </thead>
-                <tbody class="text-center">
-                    <?php require_once './data_fetch.php' ?>
-                    <div class="table-responsive">
-                        <tr>
-                            <td>
-                                <input class="form-control-plaintext form-control-sm" type="date" name="date">
-                            </td>
-                            <td>
-                                <input class="form-control-plaintext form-control-sm" type="text" name="acname">
-                            </td>
-                            <td>
-                                <select class="form-control-plaintext form-control-sm" type="text" name="bank">
-                                    <option>State Bank Of India</option>
-                                    <option>Union Bank</option>
-                                    <option>Canara Bank</option>
-                                    <option>Federal Bank</option>
-                                    <option>Punjab National Bank</option>
-                                    <option>Yes Bank</option>
-                                    <option>Axis Bank</option>
-                                    <option>HDFC Bank</option>
-                                    <option>ICICI Bank</option>
-                                </select>
-                            </td>
-                            <td>
-                                <select class="form-control-plaintext form-control-sm" type="text" name="pay_method">
-                                    <option>UPI</option>
-                                    <option>Transfer</option>
-                                    <option>Cheque</option>
-                                    <option>Credit Card</option>
-                                    <option>Debit Card</option>
-                                    <option>Netbanking</option>
-                                    <option>RTGS</option>
-                                </select>
-                            </td>
-                            <td>
-                                <input class="form-control-plaintext form-control-sm" type="text" name="trans_id">
-                            </td>
-                            <td>
-                                <input class="form-control-plaintext form-control-sm" type="text" name="amount">
-                            </td>
-                        </tr>
-                    </div>
-                </tbody>
-            </table>
-            <div id="button-container">
-                <div id="center" class="text-center">
-                    <button id="submit-button" class="btn btn-outline-success" formaction="./"
-                        type="submit">Submit</button>
-                </div>
+    <div class="container">
+        <form action="./db_insert.php" method="post" class="was-validated form-group" onsubmit="return validate()">
+            <div class="mb-3 mt-3">
+                <label for="uname" class="form-label">Date Of Transaction</label>
+                <input type="date" name="date" class="form-control" required>
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="mb-3 mt-3">
+                <label for="uname" class="form-label">Name</label>
+                <input type="text" name="acname" class="form-control" placeholder="Enter Your Name Here" required>
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="mb-3 mt-3">
+                <label for="uname" class="form-label">Bank Name</label>
+                <select class="form-control" type="text" name="bank">
+                    <option>State Bank Of India</option>
+                    <option>Union Bank</option>
+                    <option>Canara Bank</option>
+                    <option>Federal Bank</option>
+                    <option>Punjab National Bank</option>
+                    <option>Yes Bank</option>
+                    <option>Axis Bank</option>
+                    <option>HDFC Bank</option>
+                    <option>ICICI Bank</option>
+                </select>
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="mb-3 mt-3">
+                <label for="uname" class="form-label">Payment Method</label>
+                <select class="form-control" type="text" name="pay_method">
+                    <option>UPI</option>
+                    <option>Transfer</option>
+                    <option>Cheque</option>
+                    <option>Credit Card</option>
+                    <option>Debit Card</option>
+                    <option>Netbanking</option>
+                    <option>RTGS</option>
+                </select>
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="mb-3 mt-3">
+                <label for="uname" class="form-label">Transaction ID</label>
+                <input type="text" name="trans_id" class="form-control" placeholder="Enter The Transaction ID Here"
+                    required>
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="mb-3 mt-3">
+                <label for="uname" class="form-label">Amount in INR</label>
+                <input type="text" name="amount" class="form-control" placeholder="Enter The Transaction Amount Here"
+                    required>
+                <div class="valid-feedback">Valid.</div>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="form-check mb-3">
+                <input class="form-check-input" type="checkbox" id="myCheck" name="remember" required>
+                <label class="form-check-label" for="myCheck">I agree that the details I have given are correct</label>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </form>
     </div>
-    <br>
-    <div class="text-center">
-        <strong>After Submission please close the previous tab and refresh the tab opened after submission</strong>
-    </div>
-    <?php require_once './db_insert.php' ?>
-
-    <script src="script.js"></script>
-    <!-- <script src="./bootstrap-5.1.3/js/jquery-3.6.0.js"></script> -->
+    <script src="./script.js"></script>
     <script src="./bootstrap-5.1.3/js/bootstrap.min.js"></script>
 </body>
 
